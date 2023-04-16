f236210a190d4f873cf61478c027d71023463313
SamRnk <samreerink@gmail.com>
2023-04-16 06:50:37 +0200

    Added ft_strjoin_free

--- libft.h
+++ libft.h
@@ -81,6 +81,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 char	*ft_strjoin(char const *s1, char const *s2);
 
+char	*ft_strjoin_free(char *s1, char *s2);
+
 char	*ft_strtrim(char const *s1, char const *set);
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
