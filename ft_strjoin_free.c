c12950a18af51f846c70c1cbbd581d7917b02bf3
SamRnk <samreerink@gmail.com>
2024-02-15 02:00:55 +0100

    Added get_next_line and ft_index_chr

--- ft_strjoin_free.c
+++ ft_strjoin_free.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_strjoin.c                                       :+:    :+:            */
+/*   ft_strjoin_free.c                                 :+:    :+:             */
 /*                                                     +:+                    */
 /*   By: sreerink <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/16 16:45:11 by sreerink      #+#    #+#                 */
-/*   Updated: 2022/11/16 17:40:05 by sreerink      ########   odam.nl         */
+/*   Updated: 2024/02/15 01:35:47 by sreerink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ char	*ft_strjoin_free(char *s1, char *s2)
 
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	new_str = malloc(sizeof(char) * s1_len + s2_len + 1);
+	new_str = malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!new_str)
-		return (0);
+		return (NULL);
 	ft_strlcpy(new_str, s1, s1_len + 1);
 	free(s1);
 	s1 = NULL;
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_strlcpy.c                                       :+:    :+:            */
+/*   ft_strlcpy.c                                      :+:    :+:             */
 /*                                                     +:+                    */
 /*   By: sreerink <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/10 14:22:30 by sreerink      #+#    #+#                 */
-/*   Updated: 2022/11/28 22:34:13 by sreerink      ########   odam.nl         */
+/*   Updated: 2024/02/15 01:35:02 by sreerink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	i = 0;
 	if (size == 0)
 		return (ft_strlen(src));
-	while (src[i] != '\0' && i < (size - 1))
+	while (i < (size - 1) && src[i] != '\0')
 	{
 		dst[i] = src[i];
 		i++;
